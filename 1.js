74fab4b24dd52bec56ffe334d365f4bb420bb35a
shiquda <1847259360@qq.com>
2022-08-06 19:22:51 +0800

    test2

--- 1.js
+++ 1.js
@@ -1,1 +1,16 @@
-console.log('Hello  World')
+// ==UserScript==
+// @name         提示快写作业
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  快写作业
+// @author       shiquda
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    console.log('快写作业！！')
+})();
