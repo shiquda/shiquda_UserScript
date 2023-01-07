5904816bfb61eb39ab65ba9d4c5fa6dd3c5d29bb
shiquda <104683621+shiquda@users.noreply.github.com>
2023-01-07 12:10:05 +0800

    change

--- storage.js
+++ storage.js
@@ -1,9 +1,11 @@
 //shiquda的代码储存库
 function appendElement(type,text,func,appendTo){
-    if (type === 'btn'){
-    var a = document.createElement('button')
-    a.textContent = text
-    a.addEventListener("click",func)
-    appendTo.appendChild(a)
+    switch (type){
+        case "botton":
+            var a = document.createElement('button')
+            a.textContent = text
+            a.addEventListener("click",func)
+            appendTo.appendChild(a)
+            break
     }
 }
