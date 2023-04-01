1274dcbbc3a508775f87666e588d74851aa0afe2
shiquda <1847259360@qq.com>
2023-04-01 17:08:59 +0800

    New Script update

---
+++ Greasy Fork by-site-searching URL autoextract.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Greasy Fork by-site-searching URL autoextract
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Automatically extract the domain name from an input URL.
+// @author       shiquda
+// @match        https://greasyfork.org/*/scripts/by-site
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var tab = document.querySelector("body > div.width-constraint > section > form > input[type=search]:nth-child(2)")
+    tab.addEventListener("input",change)
+    function change(){
+        var text = tab.value
+        var change = text.match(/^https?:\/\/(?:www\.)?([^/?#]+)/i)[1]
+        tab.value = change
+    }
+    // Your code here...
+})();
