cd1b4e5f9d1b0afabedd5740c422921c8ced5059
shiquda <1847259360@qq.com>
2023-05-04 10:52:10 +0800

    update

--- Steam购买结账自动勾选同意用户协议.js
+++ Steam购买结账自动勾选同意用户协议.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Steam自动勾选同意用户协议
 // @namespace    http://tampermonkey.net/
-// @version      0.2.4
+// @version      0.2.5
 // @description  Steam自动勾选同意用户协议复选框
 // @author       shiquda
 // @include      https://store.steampowered.com/*
@@ -13,7 +13,7 @@
     //读取网址
     var $href = window.location.href
     if ($href.indexOf('checkout') != -1){
-        console.log('checkout')
+        //console.log('checkout')
         $('accept_ssa').defaultChecked = true
         var tab1 = document.querySelector(".cart_totals_area"),
         tab2 = document.querySelector("#checkout_content_review"),
@@ -22,11 +22,16 @@
         a.insertBefore(tab1,a.firstChild)
     }
     else if ($href.indexOf('inventory') != -1){
-        console.log('inventory')
+        //console.log('inventory')
         window.onload = function(){
         var marketsell = document.getElementById('market_sell_dialog_accept_ssa')
         marketsell.checked = true
         }
+    }
+    else if ($href.indexOf('store.steampowered.com/cart') != -1){
+        var tab3 = document.querySelector("div.checkout_content.cart.cart_totals")
+        var head = document.querySelector("div.page_background > div.page_content")
+        head.insertBefore(tab3,head.firstChild)
     }
 })()
 
