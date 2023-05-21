96c46d6e9a4b29ee8799abf043bebd6166157b5c
shiquda <1847259360@qq.com>
2023-05-21 15:35:17 +0800

    add license

--- 知乎数值转换/zhihu figure convert.js
+++ 知乎数值转换/zhihu figure convert.js
@@ -7,6 +7,7 @@
 // @match        https://www.zhihu.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=zhihu.com
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 
 (function() {
