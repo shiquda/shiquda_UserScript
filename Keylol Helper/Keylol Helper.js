1ff55e525d21642686bc15d92019caab6f51177d
shiquda <1847259360@qq.com>
2023-06-18 22:05:20 +0800

    update

--- Keylol Helper/Keylol Helper.js
+++ Keylol Helper/Keylol Helper.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Keylol Helper
 // @namespace    http://tampermonkey.net/
-// @version      0.2.1
+// @version      0.2.2
 // @description  Keylol Helper 提供其乐论坛多便捷功能支持，包括自动检测是否有其乐消息，
 // @author       shiquda
 // @namespace    https://github.com/shiquda/shiquda_UserScript
