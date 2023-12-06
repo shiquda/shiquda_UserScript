af20a1c3885a7eed8218a8859329de841c222ec3
shiquda <1847259360@qq.com>
2023-12-06 08:54:48 +0800

    feat(bbdc-newword-export): 增加随机打乱、排序、去重、去词组功能

--- 不背单词/bbdc-newword-export.js
+++ 不背单词/bbdc-newword-export.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         不背单词生词本导出
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.2.1
 // @description  不背单词生词本导出，目前支持自动获取单词列表并导出为txt文件。
 // @author       shiquda
 // @match        https://www.bbdc.cn/newword
@@ -20,6 +20,7 @@
 
     function createUI() {
         const btn = document.createElement('button');
+        btn.id = 'export-btn';
         btn.innerText = '导出单词为txt';
         btn.addEventListener('click', downloadWords);
         document.querySelector('.crumb-wrap').appendChild(btn);
@@ -28,18 +29,39 @@
         infotab.innerText = ``;
         infotab.style = 'margin: 10px';
         document.querySelector('.crumb-wrap').appendChild(infotab);
+
+        const options = [
+            { id: 'random-checkbox', text: '随机打乱', checked: false },
+            { id: 'order-checkbox', text: '按顺序排列', checked: false },
+            { id: 'phrase-checkbox', text: '去除词组', checked: true },
+            { id: 'unique-checkbox', text: '去重', checked: true }
+        ];
+
+        options.forEach(option => {
+            const checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.id = option.id;
+            checkbox.checked = option.checked;
+            checkbox.style = 'margin: 10px';
+
+            const label = document.createElement('label');
+            label.htmlFor = option.id;
+            label.appendChild(document.createTextNode(option.text));
+
+            document.querySelector('.crumb-wrap').appendChild(checkbox);
+            document.querySelector('.crumb-wrap').appendChild(label);
+        });
     }
 
     function downloadWords() {
+        document.querySelector('#export-btn').innerText = '正在导出...';
+        document.querySelector('#export-btn').addEventListener('click', manipulateWords);
+        document.disabled = true;
         loadWords().then(() => {
-            const txt = words.join('\n');
-            const blob = new Blob([txt], { type: 'text/plain' });
-            const url = URL.createObjectURL(blob);
-            const a = document.createElement('a');
-            a.href = url;
-            a.download = 'words.txt';
-            a.click();
-            document.querySelector('#info-tab').innerText = `导出完成！`;
+            manipulateWords();
+            document.querySelector('#info-tab').innerText = `导出完成！可以重新勾选选项设置导出格式！`;
+            document.querySelector('#export-btn').disabled = false;
+            document.querySelector('#export-btn').innerText = '重新导出';
         })
     }
 
@@ -67,6 +89,37 @@
         return Promise.all(promises); // 等待所有请求都完成
     }
 
+    function manipulateWords() {
+        const cfg = {
+            random: document.querySelector('#random-checkbox').checked,
+            order: document.querySelector('#order-checkbox').checked,
+            phrase: document.querySelector('#phrase-checkbox').checked,
+            unique: document.querySelector('#unique-checkbox').checked,
+        }
+        if (cfg.random && cfg.order) {
+            alert('随机打乱和按顺序排列不能同时选择!');
+            return;
+        }
+        if (cfg.random) {
+            words.sort(() => Math.random() - 0.5);
+        }
+        if (cfg.order) {
+            words.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+        }
+        if (cfg.phrase) {
+            words = words.filter(word => !word.includes(' '));
+        }
+        if (cfg.unique) {
+            words = [...new Set(words)];
+        }
+        const txt = words.join('\n');
+        const blob = new Blob([txt], { type: 'text/plain' });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = `words.txt`;
+        a.click();
+    }
 
 
     function init() {
